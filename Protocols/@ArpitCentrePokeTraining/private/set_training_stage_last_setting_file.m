d3b0ae99c80e11e074765544b63dfff236be027b
Arpit <arpit99ag@gmail.com>
2025-05-20 14:44:14 +0100

    loading training stage during init in ArpitCentrePokeTraining

--- Protocols/@ArpitCentrePokeTraining/private/set_training_stage_last_setting_file.m
+++ Protocols/@ArpitCentrePokeTraining/private/set_training_stage_last_setting_file.m
@@ -1,17 +1,19 @@
 function set_training_stage_last_setting_file(owner,experimenter,ratname)
 
+try
+
 global     Solo_datadir;
 
-
+rat_dir = sprintf('%s\\Settings\\%s\\%s\\',Solo_datadir,experimenter,ratname);
 % Get the latest setting file
 
-u = dir([Solo_datadir 'settings_@' owner '_' experimenter '_' ratname '*.mat']);
+u = dir([rat_dir 'settings_@' owner '_' experimenter '_' ratname '*.mat']);
 if ~isempty(u)
     [filenames{1:length(u)}] = deal(u.name);
     filenames = sort(filenames'); %#ok<UDIM> (can't use dimension argument with cell sort)
     today_date_str = regexprep(char(datetime('today','Format','yy-MM-dd')), '[^0-9]', '');
     for i=length(u):-1:1 %     search from the end back
-        file_date_num = textscan(filenames{i},[sets_or_data '_' owner '_' experimenter '_' ratname '_%n%*c.mat']);
+        file_date_num = textscan(filenames{i},['settings_@' owner '_' experimenter '_' ratname '_%n%*c.mat']);
 
         if ~isempty(file_date_num{1}) &&  file_date_num{1} <= str2double(today_date_str)
             fullname = [rat_dir filenames{i}]; %     We've found it.
@@ -28,12 +30,22 @@ end
 
 % Lets find the Handle for Training Stage
 handles = get_sphandle('owner', owner);
-get_ghandle('ParamsSection_training_stage')
+stage_handle_name = 'ParamsSection_training_stage';
 for hi= 1:length(handles)
     sph_fullname=get_fullname(handles{hi});
-    handles{hi}.value = loaded_data.saved.(sph_fullname); 
-    get_callback(updated_handles{i});
+    if strcmpi(sph_fullname,stage_handle_name)
+        handles{hi}.value = loaded_data.saved.(sph_fullname); 
+        callback(handles{i});
+        break
+    end
 end
 
+return
+
+catch
+
+    return
+
+end
 
 end
